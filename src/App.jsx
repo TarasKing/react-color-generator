44570acb160cf34a524f23f9cf574aa18c3aaba9
TarasKing <ivanovtaras@live.ru>
2023-07-23 15:42:33 -0700

    React_color_generator

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Form from "./Form";
+import ColorList from "./ColorList";
+function App() {
+  return (
+    <main>
+      <Form />
+      <ColorList />
+    </main>
+  );
+}
+
+export default App;
